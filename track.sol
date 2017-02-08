5015d6c4d3a2c276411e2896918166e076229928
sibizulu <sibiraj@lightrains.com>
2017-02-08 15:31:10 +0530

    Updating track details

--- track.sol
+++ track.sol
@@ -1,40 +1,40 @@
 pragma solidity ^0.4.0;
 
 contract Tracks {
-    
+
     address public STracksOwner;
-    
+
     // Single Track details
     struct STrack{
         uint trackId; // unique Identifier number
-        bytes32 trackName; 
-        mapping (uint => Author) Authors; 
+        bytes32 trackName;
+        mapping (uint => Author) Authors;
     }
-    
-    // Single Author details 
+
+    // Single Author details
     struct Author {
         bytes32[] authorDetails;
     }
-    
+
     Author thisAuthor;
     mapping (uint => STrack) public STracks;
-    
+
     function Tracks(){
         STracksOwner = msg.sender;
     }
-    
-    function saveTrackDetails(uint trackId, bytes32 trackName) returns (uint identifier){
+
+    function saveTrackDetails(uint trackId, bytes32 trackName,bytes32[] AuthorsDetails) returns (uint identifier){
         STracks[trackId] = STrack(trackId, trackName);
+        saveAuthorDetailsToTrack(AuthorsDetails, trackId);
     }
-    
+
     function saveAuthorDetailsToTrack(bytes32[] AuthorsDetails, uint trackId){
         STrack includeAuthor = STracks[trackId];
-        
+
         for (uint i = 0; i < AuthorsDetails.length; i++){
             Author details = thisAuthor;
             details.authorDetails.push(AuthorsDetails[i]);
             includeAuthor.Authors[i] = details;
         }
     }
-    
 }
