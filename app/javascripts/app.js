b8014105bfd709070121009227adc3fa9e075380
niksmac <hello@nikhilmohan.in>
2017-02-20 22:54:45 +0530

    UI modifications

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -2,50 +2,70 @@ var accounts
 var account
 
 window.onload = function () {
-  console.log(web3.eth.coinbase)
+  $('.ui.accordion').accordion()
+
+  // console.log(web3.eth.coinbase)
   web3.eth.getAccounts(function (err, accs) {
     if (err != null) {
       alert('There was an error fetching your accounts.')
       return
     }
-
     if (accs.length == 0) {
       alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
       return
     }
-
     accounts = accs
     account = accounts[0]
-
     refreshBalance()
   })
 }
 
+function initPopup () {
+  $('.small.modal').modal('show')
+}
+
 function refreshBalance () {
-  document.getElementById('status').innerHTML = web3.fromWei(web3.eth.getBalance(web3.eth.coinbase), 'ether')
+  var filter = web3.eth.filter({address: web3.eth.coinbase})
+
+  filter.watch(function (error, result) {
+    if (!error) {
+      console.log(result)
+    }
+  })
+
+  document.getElementById('status').innerText = web3.fromWei(web3.eth.getBalance(web3.eth.coinbase), 'ether')
 }
 
 function saveDetails () {
-  console.log('hey')
+  $('#saveme').addClass('disabled loading')
+  var owners = []
+  var iswcno = $('#iswc-no').val()
+  var songname = $('#song-name').val()
 
+  for (var i = 1; i <= 5; i++) {
+    var n = $('#name-' + i).val()
+    var e = $('#email-' + i).val()
+    var p = $('#per-' + i).val()
+    owners.push({'n': n, 'e': e, 'p': p})
+  }
+  $('input').val('')
   var meta = Tracks.deployed()
-
-  console.log(meta)
   var account_one = web3.eth.coinbase
   var account_two = web3.eth.coinbase
-  var xx = [{ni: 'k'}]
 
-  meta.saveTrackDetails(10, 'test track', xx, {from: account_one}).then(function (tx_id) {
-    // If this callback is called, the transaction was successfully processed.
-    // Note that Ether Pudding takes care of watching the network and triggering
-    // this callback.
-    console.log('Transaction successful!')
+  meta.saveTrackDetails(iswcno, songname, owners, {from: account_one}).then(function (tx_id) {
+    $('#txs').append('<li>' + prettyPrintHash(tx_id, 8) + '</li>')
+    $('#saveme').removeClass('disabled loading')
   }).catch(function (e) {
-    console.log(e)
+    $('#txs').append('<li>' + e + '</li>')
   })
 }
 
 function getDetails () {
   var meta = Tracks.deployed()
   console.log(meta.STrack[10])
+}
+
+function prettyPrintHash (hash, len) {
+  return hash.slice(0, len) + '...' + hash.slice(hash.length - len, hash.length)
 }
