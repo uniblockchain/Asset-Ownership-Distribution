6797ad56706dc3d86385b321bf5b4a386c1dfb95
sibizulu <sibiraj@lightrains.com>
2017-02-21 12:58:25 +0530

    Updating contract

--- contracts/Tracks.sol
+++ contracts/Tracks.sol
@@ -6,37 +6,23 @@ contract Tracks {
   struct STrack{
     uint trackId;
     bytes32 trackName;
-    mapping (uint => Author) Authors;
-  }
-
-  struct Author {
     bytes32[] authorDetails;
   }
 
-  Author thisAuthor;
-  mapping (uint => STrack) public STracks;
+  STrack[] public STracks;
 
   function Tracks(){
     STracksOwner = msg.sender;
   }
 
   function saveTrackDetails(uint trackId, bytes32 trackName,bytes32[] AuthorsDetails) returns (uint identifier){
-    STracks[trackId] = STrack(trackId, trackName);
-    saveAuthorDetailsToTrack(AuthorsDetails, trackId);
-  }
-
-  function saveAuthorDetailsToTrack(bytes32[] AuthorsDetails, uint trackId){
-    STrack includeAuthor = STracks[trackId];
-
-    for (uint i = 0; i < AuthorsDetails.length; i++){
-      Author details = thisAuthor;
-      details.authorDetails.push(AuthorsDetails[i]);
-      includeAuthor.Authors[i] = details;
+    if (msg.sender == STracksOwner) {
+      STracks[trackId] = STrack(trackId, trackName, AuthorsDetails);
     }
   }
 
-  function getTrackDetails(uint trackId) returns (bytes32 trackName){
+  function getTrackDetails(uint trackId) returns (bytes32,bytes32[]){
     STrack trackDetails = STracks[trackId];
-    trackName = trackDetails.trackName;
+    return (trackDetails.trackName,trackDetails.authorDetails);
   }
 }
