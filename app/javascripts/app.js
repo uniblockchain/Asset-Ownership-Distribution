2bab7883088d41b6120d55ed6d409d70665cc506
niksmac <hello@nikhilmohan.in>
2017-02-23 21:57:09 +0530

    Search Added

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,6 +1,18 @@
 var accounts
 var account
 
+$(document).on('click', '#txs li', function () {
+  var txnid = $(this).attr('data-tx')
+  getTx(txnid)
+})
+
+$(document).ready(function () {
+  $('#srchfrm').on('submit', function (e) {
+    e.preventDefault()
+    alert($('#srchfrm input').val())
+  })
+})
+
 window.onload = function () {
   $('.ui.form')
     .form({
@@ -47,22 +59,22 @@ window.onload = function () {
     source: content
   })
 
-  // getTx('0x1c2f7cbedb100ba133eacdaad06b66456ecb4a31ed4500ed9447f060f7405f32')
+  // getTx('0xa01d964a00b57c502b83690870485e0aeef2983c2ea9777570625c446ddeeebb')
 
 // console.log(web3.eth.coinbase)
-// web3.eth.getAccounts(function (err, accs) {
-//   if (err != null) {
-//     alert('There was an error fetching your accounts.')
-//     return
-//   }
-//   if (accs.length == 0) {
-//     alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
-//     return
-//   }
-//   accounts = accs
-//   account = accounts[0]
-//   refreshBalance()
-// })
+  web3.eth.getAccounts(function (err, accs) {
+    if (err != null) {
+      alert('There was an error fetching your accounts.')
+      return
+    }
+    if (accs.length == 0) {
+      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
+      return
+    }
+    accounts = accs
+    account = accounts[0]
+    refreshBalance()
+  })
 }
 
 function getTx (txid) {
@@ -70,16 +82,13 @@ function getTx (txid) {
     if (errr) {
       console.log('Error ' + errr)
     }
+    console.log(ress)
     var str = web3.toAscii(ress.input)
-    var prettyStr = JSON.stringify(str, null, 2)
+
     $('.ui.modal').modal('show')
     $('#mhead').text('Tx. ' + txid)
-    $('#respData').html('<pre>' + JSON.stringify(ress, null, 2) + '</pre>')
+    $('#respData').html('<pre>' + str + '</pre>')
   })
-}
-
-function initPopup () {
-  $('.ui.modal').modal('show')
 }
 
 function refreshBalance () {
@@ -94,13 +103,12 @@ function refreshBalance () {
   document.getElementById('status').innerText = web3.fromWei(web3.eth.getBalance(web3.eth.coinbase), 'ether')
 }
 
-
 function saveDetails () {
-  // $('#saveme').addClass('disabled loading')
+  $('#saveme').addClass('disabled loading')
   var owners = []
   var totp = 0
-  var iswcno = $('#iswc-no').val()
-  var songname = $('#song-name').val()
+  var iswcno = $('#iswc-no').val().trim()
+  var songname = $('#song-name').val().trim()
 
   for (var i = 1; i <= 5; i++) {
     var n = $('#name-' + i).val()
@@ -110,10 +118,10 @@ function saveDetails () {
     totp += p
     owners.push({'n': n, 'e': e, 'i': isni, 'p': p})
   }
-  if (totp < 100) {
-    $('.error.message').text('Shares cannot be more than 100')
-    return
-  }
+  // if (totp < 100) {
+  //   $('.error.message').text('Shares cannot be more than 100')
+  //   return
+  // }
 
   $('input').val('')
 
@@ -123,7 +131,6 @@ function saveDetails () {
   meta.saveTrackDetails(iswcno, songname, owners, {from: account_one}).then(function (tx_id) {
     console.log(tx_id)
     $('#txs').append('<li data-tx="' + tx_id + '">' + prettyPrintHash(tx_id, 8) + '</li>')
-    initPopup()
     $('#saveme').removeClass('disabled loading')
   }).catch(function (e) {
     $('#txs').append('<li>' + e + '</li>')
@@ -139,27 +146,26 @@ function prettyPrintHash (hash, len) {
   return hash.slice(0, len) + '...' + hash.slice(hash.length - len, hash.length)
 }
 
-$(document).on('change', 'input.percentage', function() {
-  var sum = 0;
-  var limit = 0;
-  $('input.percentage').each(function() {
+$(document).on('change', 'input.percentage', function () {
+  var sum = 0
+  var limit = 0
+  $('input.percentage').each(function () {
     if (limit == 1) {
-      $(this).val(0);
-      $(this).attr("disabled","disabled");
+      $(this).val(0)
+      $(this).attr('disabled', 'disabled')
     } else {
-       $(this).removeAttr('disabled');
-      var thisval = Number($(this).val());
-      sum += thisval;
-      if (sum > 100){
-        sum = (sum - thisval);
-        $(this).val((100 - sum));
-        sum = 100;
-        limit = 1;
+      $(this).removeAttr('disabled')
+      var thisval = Number($(this).val())
+      sum += thisval
+      if (sum > 100) {
+        sum = (sum - thisval)
+        $(this).val((100 - sum))
+        sum = 100
+        limit = 1
       }
-      if (sum == 100){
-        limit = 1;
+      if (sum == 100) {
+        limit = 1
       }
-    } 
-  });
-});
-
+    }
+  })
+})
