3538a68f9ce14e9425e4f9356f843951d33651af
niksmac <hello@nikhilmohan.in>
2017-06-19 13:24:04 +0530

    Incomplete commit 😧

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -329,56 +329,66 @@ function loadTrackdataReport (iswcNo, total) {
   })
 }
 function drawGraph (jsonStats) {
-  Highcharts.chart('theGraph', {
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie'
-    },
-    title: {
-      text: 'Musicteam Rolaty Chart'
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false
-        },
-        showInLegend: true
-      }
-    },
-    series: [{
-      name: 'Brands',
-      colorByPoint: true,
-      data: [{
-        name: 'Microsoft Internet Explorer',
-        y: 56.33
-      }, {
-        name: 'Chrome',
-        y: 24.03,
-        sliced: true,
-        selected: true
-      }, {
-        name: 'Firefox',
-        y: 10.38
-      }, {
-        name: 'Safari',
-        y: 4.77
-      }, {
-        name: 'Opera',
-        y: 0.91
-      }, {
-        name: 'Proprietary or Undetectable',
-        y: 0.2
-      }]
-    }]
-  })
+  for (i = 0; i < jsonStats.owners.length; i++) {
+    var container = document.createElement('div')
+    document.body.appendChild('')
+    var thisgraph = 'graph' + i
+    $('#container-graph').append('<div id="' + thisgraph + '"></div>')
+
+    console.log(thisgraph);
+
+    // Highcharts.chart({
+    //   chart: {
+    //     plotBackgroundColor: null,
+    //     plotBorderWidth: null,
+    //     plotShadow: false,
+    //     type: 'pie'
+    //   },
+    //   title: {
+    //     text: 'Musicteam Rolaty Chart for ' +
+    //   },
+    //   tooltip: {
+    //     pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    //   },
+    //   plotOptions: {
+    //     pie: {
+    //       allowPointSelect: true,
+    //       cursor: 'pointer',
+    //       dataLabels: {
+    //         enabled: false
+    //       },
+    //       showInLegend: true
+    //     }
+    //   },
+    //   series: [{
+    //     name: 'Brands',
+    //     colorByPoint: true,
+    //     data: [{
+    //       name: 'Microsoft Internet Explorer',
+    //       y: 56.33
+    //     }, {
+    //       name: 'Chrome',
+    //       y: 24.03,
+    //       sliced: true,
+    //       selected: true
+    //     }, {
+    //       name: 'Firefox',
+    //       y: 10.38
+    //     }, {
+    //       name: 'Safari',
+    //       y: 4.77
+    //     }, {
+    //       name: 'Opera',
+    //       y: 0.91
+    //     }, {
+    //       name: 'Proprietary or Undetectable',
+    //       y: 0.2
+    //     }]
+    //   }]
+    // })
+  }
 }
+
 function countHolders (holder) {
   return $('#thisfrm').serializeArray().reduce(function (obj, item) {
     if (item.name.indexOf('holder') !== -1) {
