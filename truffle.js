f378e96c60b89890da58754fc525bc08fb973b8b
sibizulu <sibiraj@lightrains.com>
2018-01-03 10:31:02 +0530

    Adding config file

---
+++ truffle.js
@@ -0,0 +1,22 @@
+module.exports = {
+  networks: {
+    development: {
+      host: '13.82.93.180',
+      port: 8545,
+      gas: 4700000,
+      network_id: '10101010'
+    },
+    locals: {
+      host: '192.168.27.101',
+      port: 8545,
+      network_id: '*'
+    },
+    light: {
+      host: 'eth.lightrains.com',
+      port: 80,
+      network_id: '*'
+    }
+  }
+};
+
+// web3.eth.getBlock('pending', function (error, result) { console.log(error, result) } )
