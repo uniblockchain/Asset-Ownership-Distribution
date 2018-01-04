4cdf2ba6d9acbe97a1bca602a21a4ff20ba4aaf6
sruthi23 <sruthi2525@gmail.com>
2018-01-04 15:53:29 +0530

    Bug fixed : accepting null parameters

--- contracts/Trackdata.sol
+++ contracts/Trackdata.sol
@@ -43,6 +43,8 @@ contract Trackdata {
 	}
 
 	function saveTrackDetails(bytes32 index, string _trackDetails) public {
+		
+		require(int(index) != 0 && bytes(_trackDetails).length != 0);
 		require(track[index].used != true);
 		track[index].details = _trackDetails;
 		track[index].used = true;
